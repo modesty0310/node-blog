299ba444a9641498682e8113ca1dea137c9962e2
modesty0310 <rlatprua4@gmail.com>
2021-12-01 20:59:26 +0900

    edge template engine

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const path = require('path')
-
+const path = require('path');
 const express = require('express');
-
-const app = new express();
+const app = express();
+const { config, engine } = require('express-edge');
 
 app.use(express.static('public'));
+app.use(engine);
+
+app.set('views', `${__dirname}/views`)
 
 app.get('/', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-})
+  res.render('index')
+});
 
+app.get('/about', (req,res) => {
+  res.render('about')
+});
+
+app.get('/post', (req,res) => {
+  res.render('post')
+});
+
+app.get('/contact', (req,res) => {
+  res.render('contact')
+});
 app.listen(4000||port, () => {
 
   console.log("App listen on port 4000");
