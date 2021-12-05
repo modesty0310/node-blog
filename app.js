dc5c986e3eb9c5c77da0f1908417627221bafb24
modesty0310 <rlatprua4@gmail.com>
2021-12-05 15:56:20 +0900

    post create

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const { config, engine } = require('express-edge');
 const mongoose = require('mongoose');
 
+const Post = require('./database/models/Post');
+
 mongoose.connect('mongodb://localhost:27017/node-js-blog', {
     useNewUrlParser: true,
   })
@@ -11,9 +13,12 @@ mongoose.connect('mongodb://localhost:27017/node-js-blog', {
   .catch((err) => console.log(err));
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(engine);
 
-app.set('views', `${__dirname}/views`)
+app.set('views', `${__dirname}/views`);
+
 
 app.get('/', (req,res) => {
   res.render('index')
@@ -27,13 +32,20 @@ app.get('/post', (req,res) => {
   res.render('post')
 });
 
+app.get('/posts/new', (req, res) => {
+  res.render('create')
+});
+
+app.post('/posts/store', (req, res) => {
+  Post.create(req.body, (err, post) => {
+    res.redirect('/');
+  });
+});
+
 app.get('/contact', (req,res) => {
   res.render('contact')
 });
 
-app.get('/post/new', (req, res) => {
-  res.render('create')
-});
 
 app.listen(4000||port, () => {
 
