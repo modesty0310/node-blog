f07c69aaba8d37beb6f09a726fbb6481644b4d47
modesty0310 <rlatprua4@gmail.com>
2021-12-06 19:04:26 +0900

    fileupload req.files가 undefined

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const { config, engine } = require('express-edge');
 const mongoose = require('mongoose');
+const fileupload = require('express-fileupload');
 
 const Post = require('./database/models/Post');
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://localhost:27017/node-js-blog', {
   .then(() => console.log("connected to the database"))
   .catch((err) => console.log(err));
 
+app.use(fileupload());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -44,9 +46,13 @@ app.get('/posts/new', (req, res) => {
 });
 
 app.post('/posts/store', (req, res) => {
-  Post.create(req.body, (err, post) => {
-    res.redirect('/');
-  });
+  console.log(req.file);
+  // const { image } = req.files;
+  // image.mv(path.resolve(__dirname, 'public/posts', image.name), err => {
+  //   Post.create(req.body, (err, post) => {
+  //     res.redirect('/');
+  //   });
+  // });
 });
 
 app.get('/contact', (req,res) => {
