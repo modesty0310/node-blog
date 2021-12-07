e1751d2f05ff9a41a486f9cce0468c1736332a45
modesty0310 <rlatprua4@gmail.com>
2021-12-07 19:43:45 +0900

    fileupload multer로 교체

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 const { config, engine } = require('express-edge');
 const mongoose = require('mongoose');
-const fileupload = require('express-fileupload');
-
+const multer = require('multer');
 const Post = require('./database/models/Post');
 
 mongoose.connect('mongodb://localhost:27017/node-js-blog', {
@@ -13,13 +12,26 @@ mongoose.connect('mongodb://localhost:27017/node-js-blog', {
   .then(() => console.log("connected to the database"))
   .catch((err) => console.log(err));
 
-app.use(fileupload());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(engine);
 
 app.set('views', `${__dirname}/views`);
+
+// multer middleware
+let storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/uploads');
+  },
+  filename : function(req, file, cb){
+    cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname);
+  },
+})
+
+let upload = multer({
+  storage : storage
+}).single('image');
 
 
 app.get('/', async (req,res) => {
@@ -45,14 +57,10 @@ app.get('/posts/new', (req, res) => {
   res.render('create')
 });
 
-app.post('/posts/store', (req, res) => {
-  console.log(req.file);
-  // const { image } = req.files;
-  // image.mv(path.resolve(__dirname, 'public/posts', image.name), err => {
-  //   Post.create(req.body, (err, post) => {
-  //     res.redirect('/');
-  //   });
-  // });
+app.post('/posts/store', upload, (req, res) => {
+  Post.create(req.body, (err, post) => {
+    res.redirect('/');
+  });
 });
 
 app.get('/contact', (req,res) => {
