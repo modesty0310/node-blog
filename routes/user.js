ebf046127133f0f9b560fdb804a258ef23591520
modesty0310 <rlatprua4@gmail.com>
2021-12-23 21:36:55 +0900

    comment update view 꾸미는중

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const router = express.Router()
-const {createUser, login, logout} = require('../controllers/userAPI');
-const { isLoggedIn ,isNotLoggedIn } = require('../middleware/loginChecked');
+const express = require("express");
+const router = express.Router();
+const { createUser, login, logout } = require("../controllers/userAPI");
+const { isLoggedIn, isNotLoggedIn } = require("../middleware/loginChecked");
 
-router.get('/register', isNotLoggedIn, createUser);
-router.post('/register', isNotLoggedIn, createUser);
-router.get('/login', isNotLoggedIn, login);
-router.post('/login', isNotLoggedIn, login);
-router.get('/logout', isLoggedIn, logout);
-
+router.get("/register", isNotLoggedIn, createUser);
+router.post("/register", isNotLoggedIn, createUser);
+router.get("/login", isNotLoggedIn, login);
+router.post("/login", isNotLoggedIn, login);
+router.get("/logout", isLoggedIn, logout);
 
 module.exports = router;
